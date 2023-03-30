2390686e6203cd1ffb47f2b9acc166015699740f
Daniel Farias <daniel.dsfarias@gmail.com>
2023-03-30 11:13:30 -0300

    add nprogress

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 // Composables
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
@@ -22,5 +24,16 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 })
+
+router.beforeResolve((to, from, next) => {
+  if (to.name) {
+      NProgress.start();
+  }
+  next();
+});
+
+router.afterEach((to, from) => {
+  NProgress.done()
+});
 
 export default router
