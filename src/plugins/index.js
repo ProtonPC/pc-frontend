b329a3c03052d2e9d40754c1bc173801f8efd4de
Bruno Oliveira <bds.commus@gmail.com>
2023-04-26 00:59:16 -0300

    feat: create custom datatable

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -9,11 +9,14 @@ import { loadFonts } from './webfontloader'
 import vuetify from './vuetify'
 import pinia from '../store'
 import router from '../router'
+import { registerComponents } from "@/plugins/components";
+
 
 export function registerPlugins (app) {
   loadFonts()
   app
-    .use(vuetify)
-    .use(router)
-    .use(pinia)
+  .use(vuetify)
+  .use(router)
+  .use(pinia)
+  registerComponents(app)
 }
