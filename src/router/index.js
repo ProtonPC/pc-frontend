a49dc276612937bd950129e781e70c46b57e45ff
Bruno Oliveira <bds.commus@gmail.com>
2023-04-27 13:16:58 -0300

    fix: remane NewEditProductType to ProductTypeForm

--- src/router/index.js
+++ src/router/index.js
@@ -26,11 +26,11 @@ const routes = [
       },
       {
         path: 'product-types/:id',
-        name: 'NewEditProductType',
+        name: 'ProductTypeForm',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/ProductTypes/NewEditProductType.vue'),
+        component: () => import(/* webpackChunkName: "home" */ '@/views/ProductTypes/ProductTypeForm.vue'),
       },
       {
         path: 'products',
