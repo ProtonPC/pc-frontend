571e2a2ffa9016b73022767e2bfb7a091607fd09
Bruno Oliveira <bds.commus@gmail.com>
2023-04-27 13:51:05 -0300

    feat: add Ports router

--- src/router/index.js
+++ src/router/index.js
@@ -33,6 +33,14 @@ const routes = [
         component: () => import(/* webpackChunkName: "home" */ '@/views/ProductTypes/ProductTypeForm.vue'),
       },
       {
+        path: 'ports',
+        name: 'Ports',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Ports/Ports.vue'),
+      },
+      {
         path: 'products',
         name: 'Products',
         // route level code-splitting
