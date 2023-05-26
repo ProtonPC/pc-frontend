1dc478f5baf82c80c776b0e7f0b680991d88e6fe
Bruno Oliveira <bds.commus@gmail.com>
2023-05-25 21:57:30 -0300

    feat: make ports manage data from mirage

--- src/views/Ports/Ports.vue
+++ src/views/Ports/Ports.vue
@@ -16,10 +16,13 @@
     <my-data-table
       v-model:headers="headers"
       v-model:items="items"
+      :onDelete="onDelete"
+      :onEdit="onEdit"
     ></my-data-table>
   </div>
 </template>
 <script>
+import { getPorts, deletePort } from '@/services/ports';
 export default {
   data() {
     return {
@@ -27,92 +30,26 @@ export default {
       items: [],
     };
   },
-  mounted() {
-    this.loadData();
+  async mounted() {
+    this.headers = {
+        name: "Name",
+        country: "Country",
+        state: "State",
+      },
+    await this.loadData();
   },
   methods: {
-    loadData() {
-      (this.headers = {
-          name: "Name",
-          phone: "Location",
-          country: "Country",
-          zipcode: "Zip Code",
-          state: "State",
-        }
-      ),
-        (this.items = [
-          {
-            name: "John Doe",
-            phone: "+1-202-555-0163",
-            country: "United States",
-            zipcode: "20001",
-            state: "District of Columbia",
-          },
-          {
-            name: "Jane Smith",
-            phone: "+44-20-5555-0190",
-            country: "United Kingdom",
-            zipcode: "SW1A 2AA",
-            state: "London",
-          },
-          {
-            name: "Juan Perez",
-            phone: "+52-55-5555-0191",
-            country: "Mexico",
-            zipcode: "06000",
-            state: "Mexico City",
-          },
-          {
-            name: "Maria Garcia",
-            phone: "+34-91-555-0192",
-            country: "Spain",
-            zipcode: "28001",
-            state: "Madrid",
-          },
-          {
-            name: "Hiroshi Tanaka",
-            phone: "+81-3-5555-0193",
-            country: "Japan",
-            zipcode: "100-0005",
-            state: "Tokyo",
-          },
-          {
-            name: "Lena Müller",
-            phone: "+49-30-5555-0194",
-            country: "Germany",
-            zipcode: "10178",
-            state: "Berlin",
-          },
-          {
-            name: "Francesco Rossi",
-            phone: "+39-06-5555-0195",
-            country: "Italy",
-            zipcode: "00186",
-            state: "Rome",
-          },
-          {
-            name: "Johan Svensson",
-            phone: "+46-8-5555-0196",
-            country: "Sweden",
-            zipcode: "111 57",
-            state: "Stockholm",
-          },
-          {
-            name: "Chen Wei",
-            phone: "+86-10-5555-0197",
-            country: "China",
-            zipcode: "100006",
-            state: "Beijing",
-          },
-          {
-            name: "Ravi Patel",
-            phone: "+91-22-5555-0198",
-            country: "India",
-            zipcode: "400001",
-            state: "Mumbai",
-          },
-        ]);
+    async loadData() {
+      this.items = await getPorts();
     },
+    onEdit(id){
+      this.$router.push("/ports/"+id)
+    },
+    async onDelete(id){
+      await deletePort(id)
+      console.log('delete: ' + id)
+      window.location.reload()
+    }
   },
 };
 </script>
