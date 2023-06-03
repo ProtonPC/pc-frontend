b8ffe7043854f94f3ba5b187ddbd511ed9537880
Bruno Oliveira <bds.commus@gmail.com>
2023-06-02 21:47:30 -0300

    feat: create users crud

--- src/router/index.js
+++ src/router/index.js
@@ -112,6 +112,14 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Users/Users.vue'),
       },
+      {
+        path: 'users/:id',
+        name: 'UserForm',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Users/UserForm.vue'),
+      },
     ],
   },
 ]
