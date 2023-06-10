32c834b68f283671228e627b5e637eb19b708420
Bruno Oliveira <bds.commus@gmail.com>
2023-06-09 21:03:57 -0300

    add user group to user form

--- src/views/Users/UserForm.vue
+++ src/views/Users/UserForm.vue
@@ -16,50 +16,60 @@
       {{ $route.params.id === 'new' ? 'Add' : 'Edit' }} User
     </h3>
     <v-sheet class="mx-auto">
-    <v-form @submit.prevent="submit">
-      <v-row>
-        <v-col cols="6">
-          <v-text-field
-            v-model="user.name"
-            label="User Name"
-          ></v-text-field>
-        </v-col>
-        <v-col cols="6">
-          <v-text-field
-            v-model="user.email"
-            label="User Email"
-          ></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="6">
-          <v-text-field
-            v-model="user.password"
-            :type="passwordHidden ? 'password' : 'text' "
-            :append-icon="!passwordHidden ? 'mdi-eye' : 'mdi-eye-off'"
-            @click:append="passwordHidden = !passwordHidden"
-            label="User Password"
-          ></v-text-field>
-        </v-col>
-        <v-col cols="6">
-          <v-text-field
-            v-model="user.password_confirmation"
-            :type="passwordConfirmationHidden ? 'password' : 'text'"
-            :append-icon="!passwordConfirmationHidden ? 'mdi-eye' : 'mdi-eye-off'"
-            @click:append="passwordConfirmationHidden = !passwordConfirmationHidden"
-            label="Password Confirmation"
-          ></v-text-field>
-        </v-col>
-      </v-row>
-      <v-btn @click="submit()" color="primary" class="mt-2">Submit</v-btn>
-      <v-btn @click="submitAndCreateNew()" color="secondary" class="ms-5 mt-2">Save and add another</v-btn>
-      <v-btn @click="submitAndEdit()" color="secondary" class="ms-5 mt-2">Save and continue editing</v-btn>
-    </v-form>
-  </v-sheet>
+      <v-form @submit.prevent="submit">
+        <v-row>
+          <v-col cols="4">
+            <v-text-field
+              v-model="user.name"
+              label="User Name"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              v-model="user.email"
+              label="User Email"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-select
+              label="User Group"
+              v-model="user.user_group_id"
+              :items="userGroups"
+              item-value="id"
+              item-title="name"
+            ></v-select>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="6">
+            <v-text-field
+              v-model="user.password"
+              :type="passwordHidden ? 'password' : 'text' "
+              :append-icon="!passwordHidden ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="passwordHidden = !passwordHidden"
+              label="User Password"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="6">
+            <v-text-field
+              v-model="user.password_confirmation"
+              :type="passwordConfirmationHidden ? 'password' : 'text'"
+              :append-icon="!passwordConfirmationHidden ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="passwordConfirmationHidden = !passwordConfirmationHidden"
+              label="Password Confirmation"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-btn @click="submit()" color="primary" class="mt-2">Submit</v-btn>
+        <v-btn @click="submitAndCreateNew()" color="secondary" class="ms-5 mt-2">Save and add another</v-btn>
+        <v-btn @click="submitAndEdit()" color="secondary" class="ms-5 mt-2">Save and continue editing</v-btn>
+      </v-form>
+    </v-sheet>
   </div>
 </template>
 <script>
 import { saveUser } from '@/services/users';
+import { getUserGroups } from '@/services/userGroups';
 import apiRoutes from '@/config/apiRoutes';
 import httpClient from '@/config/httpClient';
 
@@ -69,6 +79,7 @@ export default {
       user: {},
       passwordHidden: true,
       passwordConfirmationHidden: true,
+      userGroups: [],
     };
   },
   async mounted() {
@@ -84,6 +95,7 @@ export default {
       if(this.isUpdate) {
         this.user = await this.getUser(this.$route.params.id)
       }
+      this.userGroups = await getUserGroups();
     },
     async getUser(id) {
       let response = await httpClient.get(apiRoutes.getUser(id))
