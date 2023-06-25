fec7e4f5b5a6d2fe92f39dbd8beb2edce52d4edb
Daniel Farias <daniel.dsfarias@gmail.com>
2023-06-25 09:26:19 -0300

    fix: move authentication flag to env var

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,13 @@ import NProgress from 'nprogress';
 import 'nprogress/nprogress.css'
 import { createRouter, createWebHistory } from 'vue-router'
 
-const isAuthenticated = false;
+function isAuthenticated(){
+  if(process.env.NODE_ENV == 'dev'){
+    return true
+  }
+  // TODO: check if user is logged in
+  return false
+}
 
 const routes = [
   {
@@ -214,7 +220,7 @@ router.beforeResolve((to, from, next) => {
 });
 
 router.beforeEach((to, from, next) => {
-    if (to.meta.requiresAuth && !isAuthenticated )
+    if (to.meta.requiresAuth && !isAuthenticated() )
       next({
         name: "Login",
         query: {
